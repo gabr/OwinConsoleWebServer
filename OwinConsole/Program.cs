8b6f6a58422208031b035c39c202ca2c9cc6762c
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-10-28 16:38:13 +0200

    running owin example

--- OwinConsole/Program.cs
+++ OwinConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Hosting;
 
 namespace OwinConsole
 {
@@ -10,6 +12,25 @@ namespace OwinConsole
   {
     static void Main(string[] args)
     {
+      string uri = "http://localhost:8080";
+
+      using (WebApp.Start<Settings>(uri))
+      {
+        Console.WriteLine("Server started");
+        Console.ReadKey();
+        Console.ReadKey();
+        Console.WriteLine("Server closed");
+      }
+    }
+  }
+
+  class Settings
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      app.UseWelcomePage();
+      //app.Run((ctx) => { return ctx.Response.WriteAsync("execute order 88"); });
     }
   }
 }
+
